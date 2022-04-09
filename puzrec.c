d97e9eb56dbd6ffd8983642009915a63397c5640
NittaDragon <uspemskaya@gmail.com>
2022-04-09 16:24:51 +0300

    new

--- puzrec.c
+++ puzrec.c
@@ -1,10 +1,20 @@
 # include <stdio.h>
 #define k 5
-
+void rec(int i,int* p[i],int kol,char flag)
+{
+  int* per;
+  if(*p[i]<*p[i-1])
+  {
+    per=p[i];
+    p[i]=p[i-1];
+    p[i-1]=per;
+    kol++;
+    char flag=1;
+  }
+}
 int main()
 {
   int m[k]={4,3,7,9,1};
-  int *per;
   int *p[k];
   int i;
   int kol=0;
@@ -19,15 +29,8 @@ int main()
 
   do{
     flag=0;
-    for (i=1;i<k;i++)
-      if(*p[i]<*p[i-1])
-        {
-          per=p[i];
-          p[i]=p[i-1];
-          p[i-1]=per;
-          flag=1;
-          kol++;
-        }
+    for(i=0;i<k;i++)
+      rec(i,p[i],kol,flag);
   } while (flag);
 
   printf("отсортированный массив\n");
